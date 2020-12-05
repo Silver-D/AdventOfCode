b1df2cb97b539a12393299388f4d9003f002098f
Alexander Shershukov <alex.shershukov@gmail.com>
2020-12-05 15:09:49 +0300

    Day 5

---
+++ Exercises/2020/Exercise_2020_5.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    class Exercise_2020_5 : Exercise
+    {
+        public Exercise_2020_5(byte part) : base("2020", "5", part)
+        {
+            ParseInput();
+        }
+
+        private List<Dictionary<string, int>> data = new List<Dictionary<string, int>>();
+
+        private void ParseInput()
+        {
+            int lower;
+            int upper;
+
+            int HalfRange(bool lowerHalf)
+            {
+                if (lowerHalf)
+                return (upper = (int)Math.Floor((lower + upper) / 2f));
+
+                return (lower = (int)Math.Ceiling((lower + upper) / 2f));
+            }
+
+            string[] lines = input.Split('\n');
+
+            foreach(string line in lines)
+            {
+                Dictionary<string, int> entry = new Dictionary<string, int>();
+
+                int last = 0;
+
+                lower = 0;
+                upper = 127;
+
+                for (int c = 0; c < 7; c++)
+                    last = HalfRange(line[c] == 'F');
+
+                entry.Add("row", last);
+
+                last  = 0;
+                lower = 0;
+                upper = 7;
+
+                for (int c = 7; c < 10; c++)
+                    last = HalfRange(line[c] == 'L');
+
+                entry.Add("col", last);
+
+                entry.Add("seat", entry["row"] * 8 + entry["col"]);
+
+                data.Add(entry);
+            }
+            
+        }
+
+        protected override string Part_1()
+        {
+            return data.Aggregate(1, (l, v) => (v["seat"] > l) ? v["seat"] : l).ToString();
+        }
+
+        protected override string Part_2()
+        {
+            List<int> seats = new List<int>();
+
+            data.ForEach(d => seats.Add(d["seat"]));
+
+            foreach(int seat in seats)
+            {
+                int find = seat + 2;
+
+                if (seats.Contains(find) && !seats.Contains(find - 1))
+                return (find - 1).ToString();
+            }
+
+            return "";
+        }
+    }
+}
